5421e683c408af1ebac16a489759bf82d124992a
iku <31121811+ikbal-nayem@users.noreply.github.com>
2020-04-21 23:03:38 +0600

    link added

--- handler.py
+++ handler.py
@@ -1,9 +1,9 @@
 import requests
 from bs4 import BeautifulSoup as bs
 
-url = 'https://online.freemusicdownloads.world'
+url = 'https://youtube.com'
 headers = {
-	'origin': 'https://online.freemusicdownloads.world',
+	'origin': 'https://youtube.com',
 	'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36',
 }
 
